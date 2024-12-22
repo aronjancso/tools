b1e1288ed6ae5eba4a95dbe7773b1cbeaf02dc49
Aron Jancso <aron.jancso@flowject.hu>
2024-12-22 14:44:38 +0100

    feat(scripts): lowercase and uppercase rename

---
+++ scripts/nodejs/file-rename/uppercase.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const directory = __dirname;
+const files = fs.readdirSync(directory);
+const filteredFiles = files.filter(
+  (file) => file !== path.basename(__filename)
+);
+
+filteredFiles.forEach((file) => {
+  const lowercaseName = file.toUpperCase();
+  const originalPath = path.join(directory, file);
+  const newPath = path.join(directory, lowercaseName);
+
+  if (originalPath !== newPath) {
+    try {
+      fs.renameSync(originalPath, newPath);
+      console.log(`✅ ${file} -> ${lowercaseName}`);
+    } catch (err) {
+      console.error(`❌ ${file} -> ${lowercaseName}`);
+    }
+  }
+});
